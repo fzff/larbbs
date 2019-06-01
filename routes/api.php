93a40a1b2504ae0479627fbb08eb90472ad3867d
zhaoYufeng <zhaoyf@fxqifu.com>
2019-06-01 16:36:05 +0800

    add wechat login

--- routes/api.php
+++ routes/api.php
@@ -33,5 +33,17 @@ $api->version('v1', ['namespace' => 'App\Http\Controllers\Api'], function ($api)
         // 图片验证码
         $api->post('captchas', 'CaptchasController@store')->name('api.captchas.store');
         $api->get('get-captcha', 'CaptchasController@getCaptchasImage')->name('api.get-captcha.getCaptchasImage');
+
+        // 第三方登录
+        $api->post('socials/{social_type}/authorizations', 'AuthorizationsController@socialStore')
+            ->name('api.socials.authorizations.store');
     });
 });
+
+//071LJZQz1vPKkc0jJcNz1nC9Rz1LJZQc
+
+//https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx733895b6cd4a1273&secret=2937df6ce5a310da45814d5f83b6ca01&code=071LJZQz1vPKkc0jJcNz1nC9Rz1LJZQc&grant_type=authorization_code
+//https://api.weixin.qq.com/sns/userinfo?access_token=22_f4eWQvPlVCv_aGXPiQYq_RliJfaUiOrRceofiKiHKJw41KJgGb-0qjTFYzZJa37ZVB6X-cqpNywzYUnBFGpdaQ&openid=o5Sm91WO1zTnvHTdNFzHgsw4J9uU&lang=zh_CN
+
+//22_f4eWQvPlVCv_aGXPiQYq_RliJfaUiOrRceofiKiHKJw41KJgGb-0qjTFYzZJa37ZVB6X-cqpNywzYUnBFGpdaQ
+//open_id = o5Sm91WO1zTnvHTdNFzHgsw4J9uU
